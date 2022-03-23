bda02a8ebb8751b9c868cd3dce6acfc03424335f
archawinn1 <99040938+archawinn1@users.noreply.github.com>
2022-03-23 13:43:15 -0700

    Guessing Game v2
    New update with Computer Guessing Ability

--- guessinggame.py
+++ guessinggame.py
@@ -1,4 +1,5 @@
 import random
+from re import L
 #imported random to get functions
 
 #We define a function that we have created.
@@ -27,5 +28,29 @@ def guess(x):
 """The 10 that has been inputed corresposds with the x variable that we are using to 
 define the range between the numbers that we want the user to guess. If the 10 was swtiched to 100,
 the user will have to guess between 1 and 100. For now we have set the range to 10."""
+#guess(10)
 
-guess(10)
+#Here is a program that will have the Computer guess the number between the ranges of 1 and x.
+def computer_guess(x):
+    low = 1
+    high = x
+#low and high are the varitables we will be using.
+    feedback = ''
+#feedback is what the computer will get back from the user. We set it to an empty string for now.
+    while feedback != 'c': 
+    #We use a while loop that goes on until the user types in "c" that will indicate the guess is correct.
+        if low != high: #If low and high aren't equal, the computer will generate a number between the two.
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        feedback = input(f'Is {guess} too high (H), too low (L), or correct (C)??').lower() #Added .lower() make the input lowercase,
+        #feedback is the string of three choices (h,l,c) that will dictate the next condition set.
+        if feedback == 'h': #If feedback is too high, the computer will guess 1 less with a new number.
+            high = guess - 1
+        elif feedback == 'l': #If feedback is too low, the computer will guess one higher with a new number.
+            low = guess + 1
+    
+    print(f'Yay! The computer has guessed your number, {guess}, correctly!')
+
+computer_guess(10)
+
